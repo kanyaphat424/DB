92be92f1db724a2e165e421f87d95cd823a3c2f5
kanyaphat424 <mindpink6@gmail.com>
2023-12-10 17:31:26 +0700

    5555

--- carecare/lib/main.dart
+++ carecare/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo dd',
       theme: ThemeData(
         fontFamily: "Cario",
         scaffoldBackgroundColor: kBackgroundColor,
